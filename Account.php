d07e25627bf0a61c6f3a92f4a1a29ed1039877c4
baku67 <basile08@hotmail.fr>
2023-02-10 13:59:07 +0100

    Ajout crédit/débit bancaire & int=>float (montants)

--- Account.php
+++ Account.php
@@ -4,14 +4,14 @@
 
         // Propriétés
         private string $_label;
-        private int $_initialBalance;
+        private float $balance;
         private string $_currency;
         private Owner $_owner;
 
         // Constructeur
-        public function __construct(string $label, int $initialBalance, string $currency, Owner $owner) {
+        public function __construct(string $label, float $initialBalance, string $currency, Owner $owner) {
             $this->_label = $label;
-            $this->_initialBalance = $initialBalance;
+            $this->_balance = $initialBalance;
             $this->_currency = $currency;
             $this->_owner = $owner;
             $this->_owner->addAccount($this);
@@ -21,8 +21,8 @@
         public function getLabel(): string {
             return $this->_label;
         }
-        public function getInitialBalance(): int {
-            return $this->_initialBalance;
+        public function getBalance(): float {
+            return $this->_balance;
         }
         public function getCurrency(): string {
             return $this->_currency;
@@ -34,10 +34,9 @@
         // Mutateurs
         public function setLabel(string $label) {
             $this->_label = $label;
-            return "Le label a été mis à jour.<br>";
         }
-        public function setInitialBalance(string $initialBalance) {
-            $this->_initialBalance = $initialBalance;
+        public function setBalance(float $balance) {
+            $this->_balance = $balance;
         }
         public function setCurrency(string $currency) {
             $this->_currency = $currency;
@@ -47,12 +46,15 @@
         }
 
         // Méthodes
-
-        public function addCredit(int $amount) {
-
+        public function addCredit(float $amount) {
+            $newBalance = $this->_balance += $amount;
+            echo $amount . " " . $this->getCurrency() . " ont été crédité sur le compte \"" . $this->getLabel() ."\". Nouveau solde: " . $this->getBalance() . " " . $this->getCurrency();
+            return $this->setBalance($newBalance);
         }
-        public function withdraw(int $amount) {
-
+        public function withdraw(float $amount) {
+            $newBalance = $this->_balance -= $amount;
+            echo $amount . " " . $this->getCurrency() . " ont été débité sur le compte \"" . $this->getLabel() ."\". Nouveau solde: " . $this->getBalance() . " " . $this->getCurrency();
+            return $this->setBalance($newBalance);
         }
 
         public function printOwnerFullname(): string {
@@ -60,7 +62,7 @@
         }
 
         public function __toString() {
-            return "<span style='text-decoration:underline;'>Infos Account:</span><br>Intitulé: " . $this->getLabel() . "<br>Solde initial: " . $this->getInitialBalance() . "<br>Devise: " . $this->getCurrency() . "<br>Titulaire: " . $this->printOwnerFullname();
+            return "<span style='text-decoration:underline;'>Infos Account:</span><br>Intitulé: " . $this->getLabel() . "<br>Solde initial: " . $this->getBalance() . "<br>Devise: " . $this->getCurrency() . "<br>Titulaire: " . $this->printOwnerFullname();
         }
 
     }
--- index.php
+++ index.php
@@ -3,9 +3,8 @@
     require "Account.php";
     require "Owner.php";
 
-    // $birthDate envoyé en INT (car timestamp), mais le getBirthDate() renvoi un type string (jamais de type date ducoup)
     $owner1 = new Owner ("Guénolé", "De Montaigu", "19-02-1452", "Constantinople");
-    $acc1 = new Account("Livret A", 4200, "EUR", $owner1);
+    $acc1 = new Account("Livret A", 4200.5, "EUR", $owner1);
     $acc2 = new Account("Compte épargne", 154, "DENIER", $owner1);
 
     // $owner1->setAccountList([$acc1, $acc2]);
@@ -16,3 +15,6 @@
     echo "<br><br>";
     echo $acc2;
     echo "<br><br>";
+    $acc1->addCredit(140.52);
+    echo "<br><br>";
+    $acc1->withdraw(75.02);
